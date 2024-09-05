600765ba77fe3b7557d1bb55b4b96e35d1205e60
BaraaGh <baraa_ghalayini@icloud.com>
2024-09-06 00:08:36 +0300

    Final Product

--- Backend/src/main/java/MaidsCC/Backend/dao/impl/PatronDAOImpl.java
+++ Backend/src/main/java/MaidsCC/Backend/dao/impl/PatronDAOImpl.java
@@ -16,7 +16,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
